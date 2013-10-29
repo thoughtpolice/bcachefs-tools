24e98ce46d17ebdff72467e73794c7851b2ed28f
Claudio Fleiner <claudio@fleiner.com>
2013-10-28 22:44:07 -0700

    Adding autotools support to bcache-tools
    thie enables building it in a different directory, more
    flexible install and configure options, and should make it
    easier to eventualy create deb and rpm packages. Additionally
    this makes it much easier to build and test this package
    as it now behaves the same way as others we are using.

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+export ACLOCAL_FLAGS=""
+export ACLOCAL_AMFLAGS="-I m4"
+
+aclocal $ACLOCAL_FLAGS
+
+if glibtoolize -h > /dev/null 2>&1 ; then
+   glibtoolize --copy --force
+else
+   libtoolize --copy --force
+fi
+
+autoheader
+automake --copy --add-missing --foreign -Wall -Wno-portability
+autoconf
