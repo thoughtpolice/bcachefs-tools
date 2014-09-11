2b1176d6d5ddd2515c76e360218d9dfca1a04452
Kent Overstreet <kmo@daterainc.com>
2014-09-11 15:55:39 -0700

    Add crc32c support
    Change-Id: I9f830560b25cc84d77f46e3670ed07c05f44adc9

--- bcache.h
+++ bcache.h
@@ -9,6 +9,16 @@
 
 #include <linux/bcache.h>
 
+typedef __u8	u8;
+typedef __u16	u16;
+typedef __u32	u32;
+typedef __u64	u64;
+
+typedef __s8	s8;
+typedef __s16	s16;
+typedef __s32	s32;
+typedef __s64	s64;
+
 #define SB_START		(SB_SECTOR * 512)
 
 extern const char * const cache_state[];
