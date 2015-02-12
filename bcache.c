thoughtpolice/bcachefs-tools

commit 5730e47b6f6b1463c27eb8f6bcc8c1345be50e75

Cleanup, rip out more code, fix compiler warnings
Change-Id: I76b410dc700a917d2717b1ceda0df061a1599144

bcache.c
@@ -64,6 +64,42 @@ const char * const bdev_state[] = {
 	NULL
 };
 
+const char * const set_attrs[] = {
+	"btree_flush_delay",
+	"btree_scan_ratelimit",
+	"bucket_reserve_percent",
+	"cache_reserve_percent",
+	"checksum_type",
+	"congested_read_threshold_us",
+	"congested_write_threshold_us",
+	"data_replicas",
+	"errors",
+	"foreground_target_percent",
+	"gc_sector_percent",
+	"journal_delay_ms",
+	"meta_replicas",
+	"sector_reserve_percent",
+	"tiering_percent",
+	NULL
+};
+
+const char * const cache_attrs[] = {
+	"cache_replacement_policy",
+	"discard",
+	"state",
+	"tier",
+	NULL
+};
+
+const char * const internal_attrs[] = {
+	"btree_shrinker_disabled",
+	"copy_gc_enabled",
+	"foreground_write_rate",
+	"tiering_enabled",
+	"tiering_rate",
+	NULL
+};
+
 char *skip_spaces(const char *str)
 {
 	while (isspace(*str))
@@ -583,8 +619,8 @@ void write_cache_sbs(int *fds, struct cache_sb *sb,
 			m->bucket_size = bucket_sizes[0];
 		} else {
 			if (!bucket_sizes[i]) {
-				printf("No bucket size specified for cache %d,"
-				       " using the default of %d",
+				printf("No bucket size specified for cache %zu,"
+				       " using the default of %u",
 				       i, bucket_sizes[0]);
 				m->bucket_size = bucket_sizes[0];
 			} else {
@@ -821,11 +857,6 @@ void show_super_backingdev(struct cache_sb *sb, bool force_csum)
 	       bdev_state[BDEV_STATE(sb)]);
 }
 
-static void show_cache_member(struct cache_sb *sb, unsigned i)
-{
-
-}
-
 void show_super_cache(struct cache_sb *sb, bool force_csum)
 {
 	struct cache_member *m = sb->members + sb->nr_this_dev;
@@ -866,16 +897,16 @@ void show_super_cache(struct cache_sb *sb, bool force_csum)
 	printf("cache.discard\t%llu\n",		CACHE_DISCARD(m));
 }
 
-static int __sysfs_attr_type(char *attr, const char **attr_arr) {
-	int i, j;
-	for(i = 0; attr_arr[i] != NULL; i++)
+static int __sysfs_attr_type(const char *attr, const char * const *attr_arr)
+{
+	for (unsigned i = 0; attr_arr[i] != NULL; i++)
 		if(!strcmp(attr, attr_arr[i]))
 			return 1;
 	return 0;
 }
 
-enum sysfs_attr sysfs_attr_type(char *attr) {
-	int ret;
+enum sysfs_attr sysfs_attr_type(const char *attr)
+{
 	if(__sysfs_attr_type(attr, set_attrs))
 		return SET_ATTR;
 	if(__sysfs_attr_type(attr, cache_attrs))
@@ -888,13 +919,14 @@ enum sysfs_attr sysfs_attr_type(char *attr) {
 	return -1;
 }
 
-static void __sysfs_attr_list(const char **attr_arr) {
-	int i, j;
-	for (i = 0; attr_arr[i] != NULL; i++)
+static void __sysfs_attr_list(const char * const *attr_arr)
+{
+	for (unsigned i = 0; attr_arr[i] != NULL; i++)
 		printf("%s\n", attr_arr[i]);
 }
 
-void sysfs_attr_list() {
+void sysfs_attr_list()
+{
 	__sysfs_attr_list(set_attrs);
 	__sysfs_attr_list(cache_attrs);
 	__sysfs_attr_list(internal_attrs);
@@ -968,8 +1000,8 @@ char *dev_name(const char *ugly_path) {
 	return strdup(buf);
 }
 
-static void list_cacheset_devs(char *cset_dir, char *cset_name, bool parse_dev_name) {
-	int i = 0;
+static void list_cacheset_devs(char *cset_dir, char *cset_name, bool parse_dev_name)
+{
 	DIR *cachedir, *dir;
 	struct stat cache_stat;
 	char entry[MAX_PATH];
@@ -1017,7 +1049,8 @@ static void list_cacheset_devs(char *cset_dir, char *cset_name, bool parse_dev_n
 	}
 }
 
-char *find_matching_uuid(char *stats_dir, char *subdir, const char *stats_dev_uuid) {
+char *find_matching_uuid(char *stats_dir, char *subdir, const char *stats_dev_uuid)
+{
 	/* Do a query-dev --uuid only to get the uuid
 	 * repeat on each dev until we find a matching one
 	 * append that cache# to subdir and return
@@ -1338,7 +1371,6 @@ char *read_stat_dir(DIR *dir, char *stats_dir, char *stat_name, char *ret)
 	}
 
 	if (S_ISREG(statbuf.st_mode)) {
-		char buf[MAX_PATH];
 		FILE *fp = NULL;
 
 		fp = fopen(entry, "r");
@@ -1356,7 +1388,7 @@ err:
 }
 
 char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
-		bool show_devs)
+			  bool show_devs)
 {
 	char *err = NULL;
 	char bucket_size_path[MAX_PATH];
@@ -1373,7 +1405,6 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 	double total_cap = 0, total_free = 0;
 	int precision = 2;
 
-	snprintf(intbuf, 4, "%d", i);
 	snprintf(bucket_size_path, MAX_PATH, "%s/%s/%s/%s", cset_dir,
 			capacity_uuid, "cache0", "bucket_size_bytes");
 	snprintf(nbuckets_path, MAX_PATH, "%s/%s/%s/%s", cset_dir,
@@ -1383,7 +1414,7 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 	snprintf(cache_path, MAX_PATH, "%s/%s/%s", cset_dir, capacity_uuid,
 			"cache0");
 
-	while(true) {
+	while (true) {
 		char buf[MAX_PATH];
 		int len;
 		DIR *cache_dir;
@@ -1417,6 +1448,8 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 			dev_names[dev_count] = dev_name(buf);
 		}
 
+		snprintf(intbuf, 4, "%d", dev_count);
+
 		/* remove i/stat and append i++/stat */
 		bucket_size_path[strlen(cache_path) - strlen(intbuf)] = 0;
 		nbuckets_path[strlen(cache_path) - strlen(intbuf)] = 0;
@@ -1425,7 +1458,6 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 
 		dev_count++;
 
-		snprintf(intbuf, 4, "%d", dev_count);
 		strcat(cache_path, intbuf);
 		strcat(bucket_size_path, intbuf);
 		strcat(nbuckets_path, intbuf);

bcacheadm.c
@@ -477,8 +477,6 @@ int bcache_modify(NihCommand *command, char *const *args)
 {
 	char *err;
 	char path[MAX_PATH];
-	DIR *path_dir;
-	struct stat cache_stat;
 	char *attr = args[0];
 	char *val = NULL;
 	int fd = -1;
@@ -604,7 +602,7 @@ int bcache_query_devs(NihCommand *command, char *const *args)
 
 int bcache_status(NihCommand *command, char *const *args)
 {
-	int i, dev_count = 0, seq, nr_in_set = 0;
+	int i, dev_count = 0, seq;
 	struct cache_sb *seq_sb = NULL;
 	char cache_path[MAX_PATH];
 	char *dev_names[MAX_DEVS];
@@ -624,7 +622,6 @@ int bcache_status(NihCommand *command, char *const *args)
 		if (!seq_sb || sb->seq > seq) {
 			seq = sb->seq;
 			seq_sb = sb;
-			nr_in_set = sb->nr_in_set;
 		} else
 			free(sb);
 	}
@@ -825,7 +822,6 @@ err:
 
 int bcache_set_failed(NihCommand *command, char *const *args)
 {
-	int i;
 	char *err = NULL;
 
 	if (!dev_failed_uuid) {

.gitignore
@@ -1,8 +1,5 @@
-bcache-super-show
-make-bcache
 bcacheadm
 probe-bcache
-bcachectl
 .*
 *.o
 tags

Makefile
@@ -3,40 +3,30 @@ PREFIX=/usr
 UDEVLIBDIR=/lib/udev
 DRACUTLIBDIR=/lib/dracut
 INSTALL=install
-CFLAGS+=-O2 -Wall -Werror -g -I.
+CFLAGS+=-std=gnu99 -O2 -Wall -Werror -g -I.
 
-all: bcacheadm make-bcache probe-bcache bcachectl
+all: bcacheadm probe-bcache
 
-install: bcacheadm make-bcache probe-bcache
-	$(INSTALL) -m0755 bcacheadm bcachectl $(DESTDIR)${PREFIX}/sbin/
-	$(INSTALL) -m0755 make-bcache bcachectl $(DESTDIR)${PREFIX}/sbin/
+install: bcacheadm probe-bcache
+	$(INSTALL) -m0755 bcacheadm $(DESTDIR)${PREFIX}/sbin/
 	$(INSTALL) -m0755 probe-bcache bcache-register		$(DESTDIR)$(UDEVLIBDIR)/
 	$(INSTALL) -m0644 69-bcache.rules	$(DESTDIR)$(UDEVLIBDIR)/rules.d/
 	#-$(INSTALL) -T -m0755 initramfs/hook	$(DESTDIR)/usr/share/initramfs-tools/hooks/bcache
-	if [ -d $(DESTDIR)$(DRACUTLIBDIR)/modules.d ]; \
-	then $(INSTALL) -D -m0755 dracut/module-setup.sh $(DESTDIR)$(DRACUTLIBDIR)/modules.d/90bcache/module-setup.sh; \
+	if [ -d $(DESTDIR)$(DRACUTLIBDIR)/modules.d ]; then\
+		$(INSTALL) -D -m0755 dracut/module-setup.sh $(DESTDIR)$(DRACUTLIBDIR)/modules.d/90bcache/module-setup.sh; \
 	fi
 	$(INSTALL) -m0644 -- *.8 $(DESTDIR)${PREFIX}/share/man/man8/
-#	$(INSTALL) -m0755 bcache-test $(DESTDIR)${PREFIX}/sbin/
 
 clean:
-	$(RM) -f make-bcache probe-bcache bcache-super-show bcache-test bcachectl *.o
+	$(RM) -f probe-bcache bcacheadm bcache-test *.o
 
-bcache-test: LDLIBS += `pkg-config --libs openssl`
+bcache.o: CFLAGS += `pkg-config --cflags uuid blkid`
 
+bcacheadm.o: CFLAGS += `pkg-config --cflags uuid blkid libnih`
 bcacheadm: LDLIBS += `pkg-config --libs uuid blkid libnih`
-bcacheadm: CFLAGS += `pkg-config --cflags uuid blkid libnih`
 bcacheadm: bcache.o
 
-make-bcache: LDLIBS += `pkg-config --libs uuid blkid`
-make-bcache: CFLAGS += `pkg-config --cflags uuid blkid`
-make-bcache: bcache.o
-
+probe-bcache.o: CFLAGS += `pkg-config --cflags uuid blkid`
 probe-bcache: LDLIBS += `pkg-config --libs uuid blkid`
-probe-bcache: CFLAGS += `pkg-config --cflags uuid blkid`
 
-bcache-super-show: LDLIBS += `pkg-config --libs uuid`
-bcache-super-show: CFLAGS += -std=gnu99
-bcache-super-show: bcache.o
-
-bcachectl: bcachectl.o
+bcache-test: LDLIBS += `pkg-config --libs openssl`

Makefile.am
@@ -2,42 +2,21 @@
 AUTOMAKE_OPTIONS=subdir-objects
 
 PREFIX=/usr
-AM_CFLAGS=-std=gnu99 `pkg-config --cflags uuid blkid`
-AM_LDFLAGS=`pkg-config --libs uuid blkid` -L$(top_builddir)
+AM_CFLAGS=-std=gnu99 -O2 -Wall -Werror -g
 
+bin_PROGRAMS=bcacheadm probe-bcache
 
-bin_PROGRAMS=make-bcache \
-	     probe-bcache \
-	     bcache-super-show \
-	     bcachectl \
-	     bcacheadm
-
-noinst_PROGRAMS=bcache-test
-
-lib_LIBRARIES=libbcache.a
-libbcache_a_SOURCES=bcache.c
-bcache_LDADD=libbcache.a
-
-make_bcache_SOURCES=make-bcache.c
-make_bcache_LDADD=libbcache.a
-
-probe_bcache_SOURCES=probe-bcache.c
-probe_bcache_LDADD=libbcache.a
-
-bcache_super_show_SOURCES=bcache-super-show.c
-bcache_super_show_LDADD=libbcache.a
-
-bcachectl_SOURCES=bcachectl.c
-
-bcache_test_SOURCES=bcache-test.c
-bcache_test_LDFLAGS=-lm `pkg-config --libs openssl`
-bcache_test_CFLAGS=$(AM_CFLAGS) `pkg-config --cflags openssl`
+bcache.o: CFLAGS += `pkg-config --cflags uuid blkid`
 
 bcacheadm_SOURCES=bcacheadm.c
-bcacheadm_CFLAGS=$(AM_CFLAGS) -g
-bcacheadm_LDFLAGS=$(AM_LDFLAGS) -lnih
-bcacheadm_LDADD=libbcache.a
+bcacheadm_CFLAGS=$(AM_CFLAGS) `pkg-config --cflags uuid blkid libnih`
+bcacheadm_LDFLAGS=`pkg-config --libs uuid blkid libnih`
+bcacheadm_LDADD=bcache.o
 
+probe_bcache_SOURCES=probe-bcache.c
+probe_bcache_CFLAGS=$(AM_CFLAGS) `pkg-config --cflags uuid blkid`
+probe_bcache_LDFLAGS=`pkg-config --libs uuid blkid`
+probe_bcache_LDADD=bcache.o
 
 udevrule_DATA=69-bcache.rules
 udevruledir=$(prefix)/lib/udev/rules.d
@@ -48,10 +27,7 @@ udevrdir=$(prefix)/lib/udev/
 initramfs_SCRIPTS=initramfs/bcache
 initramfsdir=$(prefix)/etc/initramfs-tools/hooks/
 
-man8_MANS= bcache-super-show.8 \
-	   make-bcache.8 \
-	   probe-bcache.8 \
-	   bcacheadm.8
+man8_MANS=bcacheadm.8 probe-bcache.8
 
 include ../../../../../../../../../../usr/share/DateraContainer/Makefile.rpm
 # some version of automake don't like absolute path names for included makefiles

bcache-super-show.8
@@ -1,11 +1,0 @@
-.TH bcache-super-show 8
-.SH NAME
-bcache-super-show \- Print the bcache superblock
-.SH SYNOPSIS
-.B bcache-super-show
-[\fB \-f]
-.I device
-.SH OPTIONS
-.TP
-.BR \-f
-Keep going if the superblock crc is invalid

bcache-super-show.c
@@ -1,89 +1,0 @@
-/*
- * Author: Gabriel de Perthuis <g2p.code@gmail.com>
- *
- * GPLv2
- */
-
-
-#define _FILE_OFFSET_BITS	64
-#define __USE_FILE_OFFSET64
-#define _XOPEN_SOURCE 500
-
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <inttypes.h>
-#include <linux/fs.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <uuid/uuid.h>
-
-#include "bcache.h"
-
-void usage()
-{
-	fprintf(stderr, "Usage: bcache-super-show [-f] <device>\n");
-}
-
-int main(int argc, char **argv)
-{
-	int o;
-	extern char *optarg;
-	struct cache_sb sb_stack, *sb = &sb_stack;
-	size_t bytes = sizeof(*sb);
-	bool force_csum = false;
-
-	while ((o = getopt(argc, argv, "f")) != EOF)
-		switch (o) {
-			case 'f':
-				force_csum = 1;
-				break;
-
-			default:
-				usage();
-				exit(1);
-		}
-
-	argv += optind;
-	argc -= optind;
-
-	if (argc != 1) {
-		usage();
-		exit(1);
-	}
-
-	int fd = open(argv[0], O_RDONLY);
-	if (fd < 0) {
-		printf("Can't open dev %s: %s\n", argv[0], strerror(errno));
-		exit(2);
-	}
-
-	if (pread(fd, sb, bytes, SB_START) != bytes) {
-		fprintf(stderr, "Couldn't read\n");
-		exit(2);
-	}
-
-	if (sb->u64s) {
-		bytes = sizeof(*sb) + sb->u64s * sizeof(uint64_t);
-		sb = malloc(bytes);
-
-		if (pread(fd, sb, bytes, SB_START) != bytes) {
-			fprintf(stderr, "Couldn't read\n");
-			exit(2);
-		}
-	}
-
-	if (!SB_IS_BDEV(sb))
-		show_super_cache(sb, force_csum);
-	else
-		show_super_backingdev(sb, force_csum);
-
-	return 0;
-}

bcache-tools.spec
@@ -20,11 +20,7 @@ make DESTDIR=%buildroot INSTALL=/usr/bin/install -C /bld/$RPM_PACKAGE_NAME insta
 
 %files
 %_bindir/bcacheadm
-%_bindir/bcachectl
-%_bindir/make-bcache
 %_bindir/probe-bcache
-%_bindir/bcache-super-show
-%_libdir/libbcache.a
 %_mandir/man8/*.gz
 %exclude %_prefix/etc/initramfs-tools/hooks/bcache
 %exclude %_prefix/lib/udev/bcache-register

commit eb1d49d3f1232e352c0013c651f6cc4b43d99403

bcacheadm: fix capacity command
regression from 5730e47b

Signed-off-by: Jacob Malevich <jam@daterainc.com>

    Issue DAT-1977

Change-Id: I598ca9e5623fde441bb842580c2494fd03316f00

bcache.c
@@ -1405,23 +1405,23 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 	double avail_buckets[MAX_DEVS];
 	char *dev_names[MAX_DEVS];
 	int dev_count = 0, i;
-	char intbuf[4];
 	double total_cap = 0, total_free = 0;
 	int precision = 2;
 
-	snprintf(bucket_size_path, MAX_PATH, "%s/%s/%s/%s", cset_dir,
-			capacity_uuid, "cache0", "bucket_size_bytes");
-	snprintf(nbuckets_path, MAX_PATH, "%s/%s/%s/%s", cset_dir,
-			capacity_uuid, "cache0", "nbuckets");
-	snprintf(avail_buckets_path, MAX_PATH, "%s/%s/%s/%s", cset_dir,
-			capacity_uuid, "cache0", "available_buckets");
-	snprintf(cache_path, MAX_PATH, "%s/%s/%s", cset_dir, capacity_uuid,
-			"cache0");
 
 	while (true) {
 		char buf[MAX_PATH];
 		int len;
 		DIR *cache_dir;
+
+		snprintf(bucket_size_path, sizeof(bucket_size_path), "%s/%s/%s%d/%s", cset_dir,
+				capacity_uuid, "cache", dev_count, "bucket_size_bytes");
+		snprintf(nbuckets_path, sizeof(nbuckets_path), "%s/%s/%s%d/%s", cset_dir,
+				capacity_uuid, "cache", dev_count, "nbuckets");
+		snprintf(avail_buckets_path, sizeof(avail_buckets_path), "%s/%s/%s%d/%s", cset_dir,
+				capacity_uuid, "cache", dev_count, "available_buckets");
+		snprintf(cache_path, sizeof(cache_path), "%s/%s/%s%d", cset_dir, capacity_uuid,
+				"cache", dev_count);
 
 		if((cache_dir = opendir(cache_path)) == NULL)
 			break;
@@ -1452,20 +1452,7 @@ char *bcache_get_capacity(const char *cset_dir, const char *capacity_uuid,
 			dev_names[dev_count] = dev_name(buf);
 		}
 
-		snprintf(intbuf, 4, "%d", dev_count);
-
-		/* remove i/stat and append i++/stat */
-		bucket_size_path[strlen(cache_path) - strlen(intbuf)] = 0;
-		nbuckets_path[strlen(cache_path) - strlen(intbuf)] = 0;
-		avail_buckets_path[strlen(cache_path) - strlen(intbuf)] = 0;
-		cache_path[strlen(cache_path) - strlen(intbuf)] = 0;
-
 		dev_count++;
-
-		strcat(cache_path, intbuf);
-		strcat(bucket_size_path, intbuf);
-		strcat(nbuckets_path, intbuf);
-		strcat(avail_buckets_path, intbuf);
 	}
 
 	printf("%-15s%-25s%-25s\n", "Device Name", "Capacity (512 Blocks)", "Free (512 Blocks)");

bcacheadm.c
@@ -270,7 +270,7 @@ static NihOption status_options[] = {
 
 static NihOption capacity_options[] = {
 	{'u', "set", N_("cache_set UUID"), NULL, "UUID", &capacity_uuid, NULL},
-	{'d', "devs", N_("dev UUID"), NULL, NULL, &capacity_devs, NULL},
+	{'d', "devs", N_("Individual device capacities"), NULL, NULL, &capacity_devs, NULL},
 	NIH_OPTION_LAST
 };
 