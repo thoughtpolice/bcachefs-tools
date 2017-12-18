538a1a3fb9e70c5484792d13b520c6c3fbedb6d4
Austin Seipp <aseipp@pobox.com>
2017-12-17 19:38:31 -0600

    nix: allow 'system' attr to be overridden
    This allows easier CI, cross compilation, and multi-system builds, as
    occasionally system must be overridden.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- nix/fetchnix.nix
+++ nix/fetchnix.nix
@@ -7,7 +7,7 @@
 { url                             # URL of the nixpkgs tarball to download
 , rev                             # The Git revision of nixpkgs to fetch
 , sha256                          # The SHA256 of the downloaded data
-, system ? builtins.currentSystem # This is overridable if necessary
+, system                          # System derivation, must be specified
 }:
 
 with {
